5e5b93281df2968d72d854eb0683c0f04595abec
gorniak <penalizacja@gmail.com>
2018-08-30 15:17:42 +0200

    Triangle second project done.

--- triangle.rb
+++ triangle.rb
@@ -14,13 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if (a==b) and (a==c) then
-    :equilateral
-  elsif (a==b) or (b==c) or (a==c) then
-    :isosceles
-  else
-    :scalene
-  end
+  if a <= 0 or b <= 0 or c <= 0 then
+    raise TriangleError, "sides can't be negative or 0"
+  elsif (a + b <= c) or (b + c <= a) or (a + c <= b) then
+    raise TriangleError, "this cannot be right"
+  else     
+    if (a==b) and (a==c) then
+        :equilateral
+      elsif (a==b) or (b==c) or (a==c) then
+        :isosceles
+      else
+        :scalene
+      end
+    end
 end
 
 # Error class used in part 2.  No need to change this code.
